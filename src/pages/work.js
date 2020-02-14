666ac05eac178b556b5c5b0ea58524efb141944c
Tyler Santos <tylersantos@Tylers-MacBook-Pro-2.local>
2020-02-14 14:58:25 -0700

    import bootstrap on work page

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import CardContainer from "../components/cardContainer"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Work = () => (
 <>
