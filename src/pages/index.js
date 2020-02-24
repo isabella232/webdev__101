98d50ccc6ebfb1d8f57725127daf5be04fb8dafa
cwtedwards <chris.e@getjobber.com>
2020-02-23 17:08:34 -0700

    Update adds a yaml and moves a some of the strings over

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,16 @@ import Header from "../components/header"
 import Banner from "../components/banner"
 import Cards from "../components/cards"
 import JumbotronContainer from "../components/jumbotron"
+import content from "../content/contentYAML.yaml"
 
 const IndexPage = () => (
   <div>
     <Header />
     <Banner />
     <Cards />
-    <JumbotronContainer 
-      title ="Employment and Experience"
-      message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    <JumbotronContainer
+      title = {content.index.title}
+      message = {content.index.content}
     />
   </div>
   )
