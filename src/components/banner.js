45571c0a4bd1224ca525361b8e7a1af162b1277b
cwtedwards <chris.e@getjobber.com>
2020-02-25 08:38:31 -0700

    Add some photos of bodie and change banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import content from '../content/contentYAML.yaml'
 
 const Banner = () => {
     return(
         <div className="banner">
             <div className="container">
                 <div className="row">
-                    <div className="banner-big-text">Tyler Santos</div>
-                    <div className="banner-small-text">A software engineering student description profile about me of some sort.</div>
+                    <div className="banner-big-text">{content.banner.name}</div>
+                    <div className="banner-small-text">{content.banner.title}</div>
                     <div className="banner-image">
                     <img src="../bwgradient.jpg" alt="fist" height="100%" width="100%"></img>
                     </div>
